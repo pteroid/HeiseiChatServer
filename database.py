126f73cfe4f6cb44490f8f8361a93095fc742213
nakano <tohokamiemitame4185@gmail.com>
2019-04-27 22:09:44 +0900

    dbの作成時に適当なメールを作成するようにした

--- database.py
+++ database.py
@@ -44,5 +44,48 @@ class KeitaiMessage(BaseModel):
 
 
 def create_tables():
+    if User.table_exists():
+        return
+
     with database:
         database.create_tables([User, PokebelMessage, KeitaiMessage])
+
+    from_user = User.create(email="hoge@ezweb.ne.jp")
+    from_user.save()
+    to_user = User.create(email="fuga@docomo.ne.jp")
+    to_user.save()
+
+    message = KeitaiMessage.create(from_user=from_user, to_user=to_user,
+                                   moji_type=MojiType.EMOJI,
+                                   title="タイトル",
+                                   content="本文")
+    message.save()
+
+    from_user = User.create(email="information@i.softban.jp")
+    from_user.save()
+
+    message = KeitaiMessage.create(from_user=from_user, to_user=to_user,
+                                   moji_type=MojiType.EMOJI,
+                                   title="【お客さま限定】10,000円割引クーポンプレゼント",
+                                   content="""
+ガラケーをご利用中のお客さま限定で、機種変更に使えるクーポンをプレゼント🎁
+
+【特別クーポン】
+機種代金が税込10,000円割引！
+
+有効期限：5月31日(金)まで
+
+対象機種など詳細はこちらをご確認ください。
+http://u.softbank.jp/CsR7dVt
+（アクセスには通信料がかかります）
+
+さらに【スマホスタート割】でガラケーから対象のスマホにすると機種代金が税込10,800円割引になります。※
+
+令和を新しいスマホで迎えよう!
+ゴールデンウィークはソフトバンク取扱店へぜひお越しください
+
+
+※ 通話基本プランの2年契約／2年契約（フリープラン）またはハートフレンド割引に加入すること。学割放題との併用はできません。
+""")
+    message.save()
+    print("sample messages created")
